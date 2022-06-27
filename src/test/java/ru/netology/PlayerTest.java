50ab280694315ff9cf44eb310b9a1a14ed6a0d6f
Kozhevnikova Maria <maskinka@bk.ru>
2022-06-28 00:02:11 +0700

    Modified PlayerTest

--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -18,6 +18,70 @@ public class PlayerTest {
         int actual = player.sumGenre(game.getGenre());
         assertEquals(expected, actual);
     }
+    @Test
+    public void SumHours() {
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game2 = store.publishGame("Нетология Квест", "Аркады");
+        Game game3 = store.publishGame("Завоеватели", "Стратегии");
 
-    // другие ваши тесты
+        Player player = new Player("Petya");
+        player.installGame(game2);
+
+        int expected = 4;
+        int actual = player.play(game2,4);
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void NoInstallGame() { //RuntimeException//
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game2 = store.publishGame("Нетология Квест", "Аркады");
+        Game game3 = store.publishGame("Завоеватели", "Стратегии");
+
+        Player player = new Player("Petya");
+        player.installGame(game);
+
+        assertThrows(Exception.class,() -> {
+            player.play(game2,1);
+        });
+    }
+
+    @Test
+    public void mostPlayerByGenre() {
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game2 = store.publishGame("Нетология Квест", "Аркады");
+        Game game3 = store.publishGame("Завоеватели", "Стратегии");
+
+        Player player = new Player("Petya");
+        player.installGame(game1);
+        player.installGame(game2);
+        player.installGame(game3);
+        player.play(game1, 3);
+        player.play(game2, 2);
+        player.play(game3, 2);
+
+        Game expected = game1;
+        Game actual = player.mostPlayerByGenre("Аркады");
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void notPlayTheGenre() {
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game2 = store.publishGame("Нетология Квест", "Аркады");
+        Game game3 = store.publishGame("Завоеватели", "Стратегии");
+
+        Player player = new Player("Petya");
+        player.installGame(game1);
+        player.installGame(game2);
+        player.installGame(game3);
+        player.play(game1, 3);
+        player.play(game2, 2);
+
+        String expected = null;
+        Game actual = player.mostPlayerByGenre("Стратегии");
+        assertEquals(expected, actual);
+    }
 }
